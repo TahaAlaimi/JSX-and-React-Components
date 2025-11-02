ef36b85e77b5b43d6e874c3d70b6c77b9882a6f7
Taha Alaimi <taha.alaimi@isban.ucar.tn>
2025-11-02 16:57:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Card } from 'react-bootstrap';
 import './App.css';
+import Name from './Name';
+import Price from './Price';
+import Description from './Description';
+import Image from './Image';
+import MyPicture from './MyPicture.jpg';
+import product from './product';
+
+<img src={product.image} alt={product.name} style={{ width: '150px' }} />
+
+
+
+const firstName = "Taha";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      <Card style={{ width: '18rem', padding: '20px', textAlign: 'center' }}>
+        <Image />
+        <Name />
+        <Price />
+        <Description />
+      </Card>
+
+      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+        <h3>
+          Hello, {firstName ? firstName : "there"}!
+        </h3>
+        {firstName && (
+          <img src={MyPicture} alt="My pic" style={{ marginTop: '10px', width: '100px', borderRadius: '50%' }} />
+        )}
+
+      </div>
     </div>
   );
 }
---
+++ src/product.js
@@ -0,0 +1,10 @@
+import headphones from './headphones.jpg'; // drag-and-dropped file
+
+const product = {
+  name: "Super Cool Headphones",
+  price: "64 DT",
+  description: "High-quality sound, comfortable fit, wireless connectivity.",
+  image: headphones // use the imported variable, not a string
+};
+
+export default product;
